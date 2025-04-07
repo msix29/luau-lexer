ce54aa67943899e9d6e7986f1439f5a999e17961
msix29 <abdulrehmanessam7567@gmail.com>
2025-04-07 18:29:38 +0300

    Add `README.md`

---
+++ README.md
@@ -0,0 +1,22 @@
+# Luau Lexer
+
+A lossless lexer for the luau programming language. Lexes source code into
+tokens while preserving all spaces and comments.
+
+## Usage
+
+```rust
+use luau_lexer::prelude::{Lexer, TokenType};
+let code = r#"local foo = "Hello, World!""#;
+let mut lexer = Lexer::new(code);
+let mut token = lexer.next_token();
+while token != TokenType::EndOfFile {
+    println!("{token:?}");
+    token = lexer.next_token();
+}
+```
+
+## Note
+
+* This lexer does not stop parsing when it finds an error
+* This lexer only lexes each token when asked to do so.
--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,4 @@
-//! # Luau Lexer
-//!
-//! A lossless lexer for the luau programming language. Lexes source code into
-//! tokens while preserving all spaces and comments.
-//!
-//! # Usage:
-//!
-//! ```rust
-//! use luau_lexer::prelude::{Lexer, TokenType};
-//!
-//! let code = r#"local foo = "Hello, World!""#;
-//! let mut lexer = Lexer::new(code);
-//!
-//! let mut token = lexer.next_token();
-//! while token != TokenType::EndOfFile {
-//!     println!("{token:?}");
-//!
-//!     token = lexer.next_token();
-//! }
-//! ```
-//!
-//! # Note
-//!
-//! * This lexer does not stop parsing when it finds an error
-//! * This lexer only lexes each token when asked to do so.
-
+#![doc = include_str!("../README.md")]
 #![deny(unsafe_code)]
 #![warn(clippy::missing_docs_in_private_items)]
 #![warn(missing_docs)]
